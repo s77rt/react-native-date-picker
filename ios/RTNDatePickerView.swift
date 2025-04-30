38b4c29061b8aa94caf74796554542e23798536e
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-04-30 03:48:22 +0100

    iOS: fix dismiss request

--- ios/RTNDatePickerView.swift
+++ ios/RTNDatePickerView.swift
@@ -15,30 +15,37 @@ struct RTNDatePickerView: View {
 
   var body: some View {
     EmptyView().fullScreenCover(isPresented: $viewModel.isOpen) {
-      VStack {
-        DatePicker("", selection: $viewModel.value, displayedComponents: [.date]).datePickerStyle(
-          .graphical
-        ).labelsHidden().onChange(
-          of: viewModel.value
-        ) {
-          onChange(viewModel.value)
-        }.frame(width: 320)
-        Divider()
-        HStack {
-          Button(
-            action: onCancel,
-            label: { Text("Cancel") }
-          )
-          Spacer()
-          Button(
-            action: onConfirm,
-            label: { Text("Confirm").bold() }
-          )
+      ZStack {
+        Color.clear.contentShape(Rectangle())
+          .onTapGesture {
+            onCancel()
+          }
+        VStack {
+          DatePicker("", selection: $viewModel.value, displayedComponents: [.date]).datePickerStyle(
+            .graphical
+          ).labelsHidden().onChange(
+            of: viewModel.value
+          ) {
+            onChange(viewModel.value)
+          }.frame(width: 320)
+          Divider()
+          HStack {
+            Button(
+              action: onCancel,
+              label: { Text("Cancel") }
+            )
+            Spacer()
+            Button(
+              action: onConfirm,
+              label: { Text("Confirm").bold() }
+            )
+          }
         }
+        .frame(width: 320)
+        .padding()
+        .presentationBackground(.ultraThinMaterial).background(
+          Color(UIColor.systemBackground).cornerRadius(20))
       }
-      .frame(width: 320)
-      .padding()
-      .presentationBackground(.clear).background(Color(UIColor.systemBackground).cornerRadius(20))
     }
   }
 }
