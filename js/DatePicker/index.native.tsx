780ae02180a41225c2511300dfb51f36ad596fcd
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-05-14 01:37:46 +0100

    simplify native js variable names

--- js/DatePicker/index.native.tsx
+++ js/DatePicker/index.native.tsx
@@ -14,69 +14,70 @@ import {
 	nativeValueToMsEpoch,
 } from "../utils/DateUtils";
 
-function DatePicker({ ref, value, onChange, min, max }: DatePickerProps) {
+function DatePicker({
+	ref,
+	value: valueProp,
+	onChange: onChangeProp,
+	min: minProp,
+	max: maxProp,
+}: DatePickerProps) {
 	const [isOpen, setIsOpen] = useState(false);
 
-	const initialDisplayedValue = useMemo(() => {
-		const date = value ?? defaultDateValue();
+	const initialValue = useMemo(() => {
+		const date = valueProp ?? defaultDateValue();
 		if (date === null) {
 			return null;
 		}
 		return nativeValueFromMsEpoch(date.getTime());
-	}, [value]);
+	}, [valueProp]);
 
-	const [displayedValue, setDisplayedValue] = useState(initialDisplayedValue);
-	const onDisplayedValueChange = useCallback(
-		(event: NativeSyntheticEvent<ChangeEvent>) => {
-			setDisplayedValue(event.nativeEvent.value);
-		},
-		[]
-	);
+	const [value, setValue] = useState(initialValue);
+	const onChange = useCallback((event: NativeSyntheticEvent<ChangeEvent>) => {
+		setValue(event.nativeEvent.value);
+	}, []);
 
 	const onConfirm = useCallback(() => {
 		const date =
-			displayedValue === null
-				? null
-				: new Date(nativeValueToMsEpoch(displayedValue));
-		onChange?.(date);
+			value === null ? null : new Date(nativeValueToMsEpoch(value));
+		onChangeProp?.(date);
 		setIsOpen(false);
-	}, [displayedValue, onChange]);
+	}, [value, onChangeProp]);
 
 	const onCancel = useCallback(() => {
-		setDisplayedValue(initialDisplayedValue);
+		setValue(initialValue);
 		setIsOpen(false);
-	}, [initialDisplayedValue]);
+	}, [initialValue]);
 
 	const range = useMemo<Range>(
 		() => ({
 			lowerBound:
-				min === undefined
+				minProp === undefined
 					? undefined
-					: nativeValueFromMsEpoch(min.getTime()),
+					: nativeValueFromMsEpoch(minProp.getTime()),
 			upperBound:
-				max === undefined
+				maxProp === undefined
 					? undefined
-					: nativeValueFromMsEpoch(max.getTime()),
+					: nativeValueFromMsEpoch(maxProp.getTime()),
 		}),
-		[min, max]
+		[minProp, maxProp]
 	);
 
 	useImperativeHandle(
 		ref,
 		() => ({
 			showPicker() {
-				setDisplayedValue(initialDisplayedValue);
+				setValue(initialValue);
 				setIsOpen(true);
 			},
 		}),
-		[initialDisplayedValue]
+		[initialValue]
 	);
 
 	return (
 		<RTNDatePickerNativeComponent
 			isOpen={isOpen}
-			value={displayedValue}
-			onChange={onDisplayedValueChange}
+			value={value}
+			onChange={onChange}
 			onConfirm={onConfirm}
 			onCancel={onCancel}
 			range={range}
