d366eb2b4bdab24a20e7f0a919c43e55b3301c29
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-05-19 15:44:51 +0100

    android: modal time picker: add title

--- android/src/main/java/com/s77rt/rtndatepicker/RTNDatePickerView.kt
+++ android/src/main/java/com/s77rt/rtndatepicker/RTNDatePickerView.kt
@@ -1,9 +1,19 @@
 package com.s77rt.rtndatepicker
 
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
@@ -11,6 +21,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import java.time.Instant
 import java.time.ZoneId
 
@@ -49,7 +63,7 @@ fun RTNDatePickerView(
         if (isInline) {
             TimePicker(state = timePickerState)
         } else if (isOpen) {
-            AlertDialog(
+            TimePickerDialog(
                 onDismissRequest = onCancel,
                 confirmButton = {
                     TextButton(onClick = onConfirm) {
@@ -61,8 +75,9 @@ fun RTNDatePickerView(
                         Text("Cancel")
                     }
                 },
-                text = { TimePicker(state = timePickerState) },
-            )
+            ) {
+                TimePicker(state = timePickerState)
+            }
         }
     } else {
         if (isInline) {
@@ -82,6 +97,49 @@ fun RTNDatePickerView(
                 },
             ) {
                 DatePicker(state = datePickerState)
+            }
+        }
+    }
+}
+
+// Based on compose/material3/material3/src/androidMain/kotlin/androidx/compose/material3/DatePickerDialog.android.kt
+@OptIn(ExperimentalMaterial3Api::class)
+@Suppress("FunctionName")
+@Composable
+fun TimePickerDialog(
+    onDismissRequest: () -> Unit,
+    confirmButton: @Composable () -> Unit,
+    dismissButton: @Composable () -> Unit,
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    BasicAlertDialog(
+        onDismissRequest = onDismissRequest,
+        modifier = Modifier.wrapContentHeight(),
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+    ) {
+        Surface(
+            shape = DatePickerDefaults.shape,
+            tonalElevation = DatePickerDefaults.TonalElevation,
+            color = DatePickerDefaults.colors().containerColor,
+        ) {
+            Column(modifier = Modifier.padding(horizontal = 24.dp, vertical = 24.dp), verticalArrangement = Arrangement.SpaceBetween) {
+                Text(
+                    text = "Select time",
+                    modifier = Modifier.padding(bottom = 20.dp),
+                    style = MaterialTheme.typography.labelMedium,
+                )
+                Box(Modifier.weight(1f, fill = false)) {
+                    this@Column.content()
+                }
+                Box(
+                    modifier =
+                        Modifier.align(Alignment.End),
+                ) {
+                    Row {
+                        dismissButton()
+                        confirmButton()
+                    }
+                }
             }
         }
     }
